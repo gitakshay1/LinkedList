db19399540bd82d437a2e3793393a99c9d616c66
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-21 20:27:26 +0530

    [Akshay]-Simple Linked list

---
+++ Linked list/Program.cs
@@ -0,0 +1,18 @@
+﻿using Linked_list;
+using System.Collections.Generic;
+
+namespace LinkedListProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list= new LinkedList();
+            Console.WriteLine("Adding Values in LinkedList");
+            list.Addnote(70);
+            list.Addnote(30);
+            list.Addnote(56);
+            list.Display();
+        }
+    }
+}
